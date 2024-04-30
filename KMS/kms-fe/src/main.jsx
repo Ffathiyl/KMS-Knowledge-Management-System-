9aa90b1eb04f1fb4a22e18d7a5a045ef0117ed4a
Ffathiyl <m.fatihalfalahi2003@gmail.com>
2024-04-30 10:46:05 +0700

    first

--- KMS/kms-fe/src/main.jsx
+++ KMS/kms-fe/src/main.jsx
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+<<<<<<< HEAD
 import "bootstrap/dist/js/bootstrap.min.js";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "@flaticon/flaticon-uicons/css/regular/rounded.css";
 import "@flaticon/flaticon-uicons/css/bold/rounded.css";
 
+=======
+>>>>>>> main
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
